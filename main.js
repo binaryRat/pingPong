a01e649fca6699e1e9e1fc85b7b70df0491a0e06
binaryRat <67596311+binaryRat@users.noreply.github.com>
2020-11-22 19:19:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-function start(){
+function start_offline(){
+    hideButtons()
     document.body.style.backgroundColor = "black"
 
     var firstBlock = document.createElement('div')
@@ -35,18 +36,18 @@ function start(){
                 player1Top += moveSpeed
                 break
             case 38:
-                console.log("Player 2 Up")
-                player2Top -= moveSpeed
-                break
-            case 40:
-                console.log("Player 2 Down")
-                player2Top += moveSpeed
-                break
-        }
-        if(player1Top < 2)  player1Top = 2
-        if(player2Top < 2)  player2Top = 2
-        if(player1Top +  firstBlock.clientHeight + 2 > window.innerHeight)  player1Top = window.innerHeight - 2 - firstBlock.clientHeight
-        if(player2Top +  secondBlock.clientHeight + 2 > window.innerHeight)  player2Top = window.innerHeight - 2 - secondBlock.clientHeight
+                console.log("Player 2 Up")                                                                         
+                player2Top -= moveSpeed                                                                            
+                break                                                                                              
+            case 40:                                                                                               
+                console.log("Player 2 Down")                                                                       
+                player2Top += moveSpeed                                                                            
+                break                                                                                              
+        }                                                                                                          
+        if(player1Top < 2)  player1Top = 2                                                                         
+        if(player2Top < 2)  player2Top = 2                                                                         
+        if(player1Top +  firstBlock.clientHeight + 2 > window.innerHeight)  player1Top = window.innerHeight - 2 - firstBlock.clientHeight                                                                                             
+        if(player2Top +  secondBlock.clientHeight + 2 > window.innerHeight)  player2Top = window.innerHeight - 2 - secondBlock.clientHeight                                                                                           
         firstBlock.style.top = player1Top + "px"
         secondBlock.style.top = player2Top + "px"
     }
@@ -76,16 +77,16 @@ function start(){
         ball.style.left = position.x + "px"
         ball.style.top = position.y + "px"
 
-
-        player1Top = position.y - 100 
+        //Autoplay **
+        /*player1Top = position.y - 100 
         player2Top = position.y - 100 
         if(player1Top < 2)  player1Top = 2
         if(player2Top < 2)  player2Top = 2
         if(player1Top +  firstBlock.clientHeight + 2 > window.innerHeight)  player1Top = window.innerHeight - 2 - firstBlock.clientHeight
         if(player2Top +  secondBlock.clientHeight + 2 > window.innerHeight)  player2Top = window.innerHeight - 2 - secondBlock.clientHeight
         firstBlock.style.top = player1Top + "px"
-        secondBlock.style.top = player2Top + "px"
-
+        secondBlock.style.top = player2Top + "px"*/
+        //Autoplay **
 
 
         var result = collisionDetection(ball,vector,firstBlock,secondBlock)
@@ -164,4 +165,83 @@ function start(){
     score.innerHTML = "0 : 0"
     document.body.appendChild(score)
 
+}
+
+function start_online(){
+    hideButtons()
+    document.body.style.backgroundColor = "black"
+
+    var bar_heigth = window.innerHeight / 100 * 30
+    var ball_heigth = window.innerHeight / 100 * 2
+
+    var firstBlock = document.createElement('div')
+    firstBlock.style.backgroundColor = "white"
+    firstBlock.style.width = "20px"
+    firstBlock.style.height = bar_heigth + "px"
+    firstBlock.style.left = "20px"
+    firstBlock.style.position = "absolute"
+
+
+    var secondBlock = document.createElement('div')
+    secondBlock.style.backgroundColor = "white"
+    secondBlock.style.width = "20px"
+    secondBlock.style.height = bar_heigth + "px"
+    secondBlock.style.right = "20px"
+    secondBlock.style.position = "absolute"
+
+    var player1Top = window.innerHeight/2 - bar_heigth /2
+    var player2Top = window.innerHeight/2 - bar_heigth /2
+    
+    firstBlock.style.top = player1Top + "px"
+    secondBlock.style.top = player2Top + "px"
+    document.body.appendChild(firstBlock)
+    document.body.appendChild(secondBlock)
+
+    var ball = document.createElement('div')
+    ball.style.backgroundColor = "white"
+    ball.style.width = ball_heigth +"px"
+    ball.style.height = ball_heigth +"px"
+    ball.style.position = "absolute"
+    document.body.appendChild(ball)
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        var response = this.responseText
+        var obj = JSON.parse(response);
+        console.log(obj)
+        firstBlock.style.top = ((obj.p1 / 100) * window.innerHeight - bar_heigth / 2) + "px"
+        secondBlock.style.top = ((obj.p2 / 100) * window.innerHeight - bar_heigth / 2) + "px"
+
+        ball.style.left = ((obj.ball_x / 100) * window.innerWidth - ball_heigth / 2)  + "px"
+        ball.style.top = ((obj.ball_y / 100) * window.innerHeight - ball_heigth / 2)  + "px"
+    };
+    xhttp.open("GET", "join", true);
+    xhttp.send();
+    
+    setInterval(function(){
+        xhttp.open("GET", "update", true);
+        xhttp.send();
+    },1000)
+    document.onkeydown = function(event){
+        var down = 0,up = 0;
+        switch(event.key){
+            case "w":
+            case "ArrowUp":
+                down = 1
+                break
+            case "s":
+            case "ArrowDown":
+                up = 1;
+                break
+
+        }
+        if(up) xhttp.open("GET", "update?top", true);
+        if(down) xhttp.open("GET", "update?bot", true);
+        xhttp.send();
+
+    }
+}
+function hideButtons(){
+    document.getElementById("button_online").style.visibility = "hidden"
+    document.getElementById("button_offline").style.visibility = "hidden"
 }
